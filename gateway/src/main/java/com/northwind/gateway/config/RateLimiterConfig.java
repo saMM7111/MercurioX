3a110d6c805c2175e8badada0158131a8cfef871
Sankalp Mehta <sammehta1000@gmail.com>
2026-05-23 02:23:28 +0530

    Add Redis rate limiter bean

--- gateway/src/main/java/com/northwind/gateway/config/RateLimiterConfig.java
+++ gateway/src/main/java/com/northwind/gateway/config/RateLimiterConfig.java
@@ -1,6 +1,8 @@
 package com.northwind.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
@@ -27,5 +29,12 @@ public class RateLimiterConfig {
                     .map(addr -> addr.getAddress().getHostAddress())
                     .defaultIfEmpty("unknown");
         };
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter(
+            @Value("${gateway.rate-limiter.replenish-rate:20}") int replenishRate,
+            @Value("${gateway.rate-limiter.burst-capacity:40}") int burstCapacity) {
+        return new RedisRateLimiter(replenishRate, burstCapacity);
     }
 }
